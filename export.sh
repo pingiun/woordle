f8c12a118d1448ec9caa2d5b3cb4a07b38a6bd2b
Jelle Besseling <jelle@pingiun.com>
2022-03-07 23:00:00 +0100

    Add pasen to js

--- export.sh
+++ export.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
+rm -rf app/js/*
+gmake build
+
 declare -A filenames
 
 for file in app/js/app*.js app/js/main*.js; do
@@ -12,7 +15,7 @@ for file in app/js/app*.js app/js/main*.js; do
 done
 
 cd html
-for html in **/*.html *.html; do
+for html in *.html; do
     mkdir -p "../app/$(dirname $html)"
     cp "$html" "../app/$html"
     for basename in "${!filenames[@]}"; do
